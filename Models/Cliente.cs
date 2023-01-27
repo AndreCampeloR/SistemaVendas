80173700240b368bbfc3da8c27cf2562e312edd6
AndreCampeloR <decocampeloreis@gmail.com>
2023-01-27 14:17:20 -0300

    criando o conexão

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -8,8 +8,8 @@ namespace SistemaVendas.Models
     public class Cliente
     {
         public int Id {get; set;}
-        public string Nome {get; set;}
-        public string Login {get; set;}
-        public string Senha {get; set;}
+        public string? Nome {get; set;}
+        public string? Login {get; set;}
+        public string? Senha {get; set;}
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using SistemaVendas.Context;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
