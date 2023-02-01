b56ba561469cc6ced13d59f582830ebd879c6869
AndreCampeloR <decocampeloreis@gmail.com>
2023-02-01 13:43:36 -0300

    criando método obter por nome

--- Repository/VendedorRepository.cs
+++ Repository/VendedorRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using SistemaVendas.Context;
+using SistemaVendas.Dto;
 using SistemaVendas.Models;
 
 namespace SistemaVendas.Repository
@@ -26,6 +27,14 @@ namespace SistemaVendas.Repository
           {
             var vendedor = _context.Vendedores.Find(id);
             return vendedor;
+          }
+
+          public List<ObterVendedorDto> ObterPorNome(string nome)
+          {
+            var vendedores = _context.Vendedores.Where(x => x.Nome.Contains(nome))
+                                                .Select(x => new ObterVendedorDto(x))
+                                                .ToList();
+            return vendedores;
           }
     }
 }
