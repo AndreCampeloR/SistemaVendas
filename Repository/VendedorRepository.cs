2a89c7ec935b3b3ef26e0cb42297f6b6343658c4
AndreCampeloR <decocampeloreis@gmail.com>
2023-02-01 10:06:52 -0300

    injetando dependência

--- Repository/VendedorRepository.cs
+++ Repository/VendedorRepository.cs
@@ -2,11 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SistemaVendas.Context;
 
 namespace SistemaVendas.Repository
 {
     public class VendedorRepository
     {
-        
+          private readonly VendasContext _context;
+
+          public VendedorRepository(VendasContext context)
+          {
+            _context = context;
+          }        
     }
 }
