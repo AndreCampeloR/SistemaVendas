4dbf2dc959240e5ac2528085799d532805b4eab2
AndreCampeloR <decocampeloreis@gmail.com>
2023-02-01 13:13:04 -0300

    criando construtor obtervendedor

--- Dto/ObterVendedorDto.cs
+++ Dto/ObterVendedorDto.cs
@@ -2,13 +2,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SistemaVendas.Models;
 
 namespace SistemaVendas.Dto
 {
     public class ObterVendedorDto
     {
+        public int Id {get; set;}
         public string? Nome {get; set;}
         public string? Login {get; set;}
-        public string? Senha {get; set;}
+
+        public ObterVendedorDto(Vendedor vendedor) 
+        {
+            Id = vendedor.Id;
+            Nome = vendedor.Nome;
+            Login = vendedor.Login;
+        }
     }
+
+    
 }
