53130a8379c5cf9701971b6c736ec86da5618f71
AndreCampeloR <decocampeloreis@gmail.com>
2023-02-01 10:45:36 -0300

    criando construtor VendedorDto

--- Dto/CadastrarVendedorDto.cs
+++ Dto/CadastrarVendedorDto.cs
@@ -7,6 +7,8 @@ namespace SistemaVendas.Dto
 {
     public class CadastrarVendedorDto
     {
-        
+        public string? Nome {get; set;}
+        public string? Login {get; set;}
+        public string? Senha {get; set;}
     }
 }
