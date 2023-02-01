1342ee68965f50dc860e097a6431f14cccf7141f
AndreCampeloR <decocampeloreis@gmail.com>
2023-02-01 14:01:20 -0300

    finalizando busca por nome

--- Controllers/VendedorController.cs
+++ Controllers/VendedorController.cs
@@ -44,5 +44,12 @@ namespace SistemaVendas.Controllers
             else
                return NotFound(new {Mesagem = "Vendedor não encontrado"});
         }
+
+        [HttpGet("ObterPorNome/{nome}")]
+        public IActionResult ObterPorNome(string nome)
+        {
+            var vendedores = _repository.ObterPorNome(nome);
+            return Ok(vendedores);
+        }
     }
 }
