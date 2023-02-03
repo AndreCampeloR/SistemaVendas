3f68a6b4f2e9796b18203afc41a389173ca9d160
AndreCampeloR <decocampeloreis@gmail.com>
2023-02-03 10:46:49 -0300

    corrigindo classe pedido

--- Dto/CadastrarPedidoDto.cs
+++ Dto/CadastrarPedidoDto.cs
@@ -3,16 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace SistemaVendas.Models
+namespace SistemaVendas.Dto
 {
-    public class Pedido
+    public class CadastrarPedidoDto
     {
-        public int Id {get; set;}
         public DateTime Data {get; set;}
         public int VendedorId {get; set;}
-        public Vendedor Vendedor {get; set;}
         public int ClienteId {get; set;}
-        public Cliente Cliente {get; set;}  
-        
     }
 }
--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao"))
 
 builder.Services.AddScoped<VendedorRepository>();
 builder.Services.AddScoped<ClienteRepository>();
+builder.Services.AddScoped<PedidoRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
