f8d0bea4f82bce22bd5cf02885a0abc1a7f522e0
AndreCampeloR <decocampeloreis@gmail.com>
2023-02-03 10:40:54 -0300

    criando pedidoRepository

---
+++ Repository/PedidoRepository.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SistemaVendas.Context;
+using SistemaVendas.Models;
+
+namespace SistemaVendas.Repository
+{
+    public class PedidoRepository
+    {
+        private readonly VendasContext _context;
+        
+        public PedidoRepository(VendasContext context)
+        {
+            _context = context;
+        }
+
+        public void Cadastrar(Pedido pedido)
+        {
+            _context.Pedidos.Add(pedido);
+            _context.SaveChanges();
+        }
+    }
+}
