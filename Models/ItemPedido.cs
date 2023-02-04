0d36581e3d7e908db15e1f5b50635a3a12d588ee
André Campelo <decocampeloreis@gmail.com>
2023-02-04 00:56:44 -0300

    atualizando classe item pedido

--- Models/ItemPedido.cs
+++ Models/ItemPedido.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SistemaVendas.Dto.itemPedido;
 
 namespace SistemaVendas.Models
 {
@@ -14,5 +15,26 @@ namespace SistemaVendas.Models
         public Servico Servico {get; set;}
         public int Quantidade {get; set;}
         public decimal Valor {get; set;}
+
+         public ItemPedido()
+        {
+
+        }
+
+        public ItemPedido(CadastrarItemPedidoDto dto)
+        {
+            PedidoId = dto.PedidoId;
+            ServicoId = dto.ServicoId;
+            Quantidade = dto.Quantidade;
+            Valor = dto.Valor;
+        }
+
+        public void MapearAtualizarItemPedidoDTO(AtualizarItemPedidoDto dto)
+        {
+            PedidoId = dto.PedidoId;
+            ServicoId = dto.ServicoId;
+            Quantidade = dto.Quantidade;
+            Valor = dto.Valor;
+        }
     }
 }
