92992d07be43243a28417e991b5b92d76dff3e7a
André Campelo <decocampeloreis@gmail.com>
2023-02-04 00:38:02 -0300

    criando obter servico dto

--- Dto/servico/ObterServicoDto.cs
+++ Dto/servico/ObterServicoDto.cs
@@ -2,11 +2,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SistemaVendas.Models;
 
 namespace SistemaVendas.Dto.servico
 {
     public class ObterServicoDto
     {
-        
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Descricao { get; set; }
+
+        public ObterServicoDto(Servico servico)
+        {
+            Id = servico.Id;
+            Nome = servico.Nome;
+            Descricao = servico.Descricao;
+        }
     }
 }
