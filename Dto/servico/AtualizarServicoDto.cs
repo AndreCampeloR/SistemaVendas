0c5ee5796bdc672a36a60f16813752239c9164cc
André Campelo <decocampeloreis@gmail.com>
2023-02-04 00:35:36 -0300

    criando Atualizar Pedido dto

--- Dto/servico/AtualizarServicoDto.cs
+++ Dto/servico/AtualizarServicoDto.cs
@@ -7,6 +7,7 @@ namespace SistemaVendas.Dto.servico
 {
     public class AtualizarServicoDto
     {
-        
+        public string? Nome { get; set; }
+        public string? Descricao { get; set; }
     }
 }
