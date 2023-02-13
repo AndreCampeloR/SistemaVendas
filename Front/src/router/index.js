ddf3bf0d89659e586717524f5d8b725133cee102
AndreCampeloR <decocampeloreis@gmail.com>
2023-02-13 15:36:42 -0300

    adicionando rotas

--- Front/src/router/index.js
+++ Front/src/router/index.js
@@ -1,22 +1,60 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+import ListarVendedor from '../views/Vendedor/ListarVendedor.vue'
+import CadastrarVendedor from '../views/Vendedor/CadastrarVendedor.vue'
+
+import ListarCliente from '../views/Cliente/CadastrarCliente.vue'
+import CadastrarCliente from '../views/Cliente/CadastrarCliente.vue'
+
+import ListarPedido from '../views/Pedido/CadastrarPedido.vue'
+import CadastrarPedido from '../views/Pedido/CadastrarPedido.vue'
+
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
 
+
+  {
+    path: '/vendedor/listar',
+    name: 'ListarVendedor',
+    component: ListarVendedor
+  },
+  {
+    path: '/vendedor/cadastrar',
+    name: 'CadastrarVendedor',
+    component: CadastrarVendedor
+  },
+
+
+  {
+    path: '/cliente/listar',
+    name: 'ListarCliente',
+    component: ListarCliente
+  },
+  {
+    path: '/cliente/cadastrar',
+    name: 'CadastrarCliente',
+    component: CadastrarCliente
+  },
+
+
+  {
+    path: '/pedido/listar',
+    name: 'ListarPedido',
+    component: ListarPedido
+  },
+  {
+    path: '/pedido/cadastrar',
+    name: 'CadastrarPedido',
+    component: CadastrarPedido
+  },
+  
+
+]
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
