2e0c136e4ad8ca1db537091806ad2c5d1eb3fd7a
André Campelo <decocampeloreis@gmail.com>
2023-02-14 20:05:02 -0300

    Criando View atualizar item

---
+++ Front/src/views/ItemPedidoView.vue/AtualizarItemView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="main-container">
+      <AtualizarItem/>
+    </div>
+  </template>
+  
+  <script>
+  import AtualizarItem from '../../components/itemPedido/AtualizarItem.vue'
+
+  export default {
+    name: 'AtualizarVendedorView',
+    components: {
+        AtualizarItem
+}
+  }
+  </script>
