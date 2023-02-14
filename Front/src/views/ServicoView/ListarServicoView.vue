7c14396cf48df7e78f9cef77da721bd279a1fe68
AndreCampeloR <decocampeloreis@gmail.com>
2023-02-14 13:53:17 -0300

    Criando view ListarServico

---
+++ Front/src/views/ServicoView/ListarServicoView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="main-container">
+      <TabelaListarServico/>
+    </div>
+  </template>
+  
+  <script>
+  import TabelaListarServico from '@/components/Servico/TabelaListarServico.vue';
+  
+  export default {
+    name: 'ListarServicoView',
+    components: {
+      TabelaListarServico
+    }
+  }
+  </script>
