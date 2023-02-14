76c44f32d4cd9a43c40088efb502a53f8227d168
AndreCampeloR <decocampeloreis@gmail.com>
2023-02-14 13:57:45 -0300

    Criando view de cadastrar serviço

---
+++ Front/src/views/ServicoView/CadastrarServicoView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="main-container">
+      <FormCadastroServico/>
+    </div>
+  </template>
+  
+  <script>
+  import FormCadastroServico from '@/components/Servico/FormCadastroServico.vue';
+
+  export default {
+    name: 'CadastrarServicoView',
+    components: {
+      FormCadastroServico
+    }
+  }
+  </script>
