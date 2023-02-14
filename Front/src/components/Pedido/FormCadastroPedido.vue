1f71dacd601a8084ed014303ee4c1f97f3c4a407
AndreCampeloR <decocampeloreis@gmail.com>
2023-02-14 16:38:25 -0300

    Criando Formulário de cadastro de pedido

---
+++ Front/src/components/Pedido/FormCadastroPedido.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="cadastro-servico">
+      <h3>Novo Servico</h3>
+    </div>
+  </template>
+  
+  <script>
+
+export default {
+    name: "FormCadastroPedido"
+  }
+  </script>
+  
+  <style>
+  </style>
