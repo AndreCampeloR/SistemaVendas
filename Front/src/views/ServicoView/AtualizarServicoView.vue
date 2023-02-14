77c1bc7228f20b4e3def476680a63c4bb688226e
AndreCampeloR <decocampeloreis@gmail.com>
2023-02-14 13:59:11 -0300

    Criando view Atualizar Serviço

---
+++ Front/src/views/ServicoView/AtualizarServicoView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="main-container">
+      <AtualizarServico/>
+    </div>
+  </template>
+  
+  <script>
+  import AtualizarServico from '../../components/Servico/AtualizarServico.vue';
+
+  export default {
+    name: 'AtualizarServicoView',
+    components: {
+    AtualizarServico
+}
+  }
+  </script>
